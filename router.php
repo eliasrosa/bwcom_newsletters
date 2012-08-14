afe3c242b213c2daf2e3127a14dbef023cc1e718
Elias da Rosa <elias@eliasdarosa.com.br>
2012-08-14 13:02:01 -0300

    Atualizado sistema de rotas

--- router.php
+++ router.php
@@ -1,10 +1,11 @@
 <?
+
 defined('BW') or die("Acesso negado!");
 
 // ADM
 bwRouter::addUrl('/newsletters');
-bwRouter::addUrl('/newsletters/task', array(), 'task');
+bwRouter::addUrl('/newsletters/task', array('type' => 'task'));
 bwRouter::addUrl('/newsletters/contatos/lista');
-bwRouter::addUrl('/newsletters/contatos/cadastro/:id', array('id'));
+bwRouter::addUrl('/newsletters/contatos/cadastro/:id', array('fields' => array('id')));
 bwRouter::addUrl('/newsletters/grupos/lista');
-bwRouter::addUrl('/newsletters/grupos/cadastro/:id', array('id'));
+bwRouter::addUrl('/newsletters/grupos/cadastro/:id', array('fields' => array('id')));
